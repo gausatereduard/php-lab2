1aaa87243e57d42d19d1b8caa8cf21db28d40a94
gausatereduard <gausatereduard@gmail.com>
2026-02-14 04:35:28 -0500

    finish

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+echo "Привет, мир!";
+
+echo "Hello, World with echo!" . "<br/>";
+print "Hello, World with print!" . "<br/>";
+
+$days = 288;
+$message = "Все возвращаются на работу!";
+
+echo $days . " дней каникул! " . $message . "<br/>";
+echo "{$days} дней каникул! Нет, {$message}" . "<br/>";
